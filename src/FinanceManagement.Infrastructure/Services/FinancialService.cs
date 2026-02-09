f5e9a003479435a8ca3929d6d636c9216c95836d
Neel Pandya <neelpandia3105@gmail.com>
2026-02-09 19:02:54 +0530

    TASK-002: Resolved Null Reference in Partner Name Concatenation Location -  Services\FinancialService.cs
    Null Reference in Partner Name Concatenation Location -  Services\FinancialService.cs

--- src/FinanceManagement.Infrastructure/Services/FinancialService.cs
+++ src/FinanceManagement.Infrastructure/Services/FinancialService.cs
@@ -78,7 +78,8 @@ public class FinancialService : IFinancialService
             var projectsCount = await _context.Projects
                 .CountAsync(p => p.ManagedByPartnerId == partner.Id);
 
-            var partnerName = partner.User?.FirstName + " " + partner.User?.LastName;
+            var fullName = partner.User?.FirstName + " " + partner.User?.LastName;
+            var partnerName = string.IsNullOrWhiteSpace(fullName) ? "N/A" : fullName;
 
             partnerIncomes.Add(new PartnerIncomeDto
             {
