bbce6d2434b66607b846ec80d3ba36bb8b943b5c
ParagWeb7 <paragweb7@gmail.com>
2026-02-11 16:27:42 +0530

    Task 05: Optimize Project Fetching Logic

--- src/FinanceManagement.Infrastructure/Repositories/ProjectRepository.cs
+++ src/FinanceManagement.Infrastructure/Repositories/ProjectRepository.cs
@@ -21,14 +21,7 @@ public class ProjectRepository : IProjectRepository
 
     public async Task<IEnumerable<Project>> GetAllAsync()
     {
-        var projects = await _context.Projects.ToListAsync();
-        
-        foreach (var project in projects)
-        {
-            project.ManagedByPartner = await _context.Partners
-                .FirstOrDefaultAsync(p => p.Id == project.ManagedByPartnerId);
-        }
-        
+        var projects = await _context.Projects.Include(p=> p.ManagedByPartner).ToListAsync();
         return projects;
     }
 
@@ -80,7 +73,6 @@ public class PartnerRepository : IPartnerRepository
 
     public async Task<Partner?> GetByIdAsync(int id)
     {
-       // id = 99;
         return await _context.Partners
             .Include(p => p.User)
             .FirstOrDefaultAsync(p => p.Id == id);
