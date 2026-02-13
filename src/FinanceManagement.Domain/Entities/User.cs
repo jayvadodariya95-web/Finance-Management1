37c131def37158d89a224f4f301e5822b07532f2
Prince3963 <princepatel3963@gmail.com>
2026-02-13 12:13:11 +0530

    Updated User model and add enum

--- src/FinanceManagement.Domain/Entities/User.cs
+++ src/FinanceManagement.Domain/Entities/User.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
 using FinanceManagement.Domain.Common;
 using FinanceManagement.Domain.Enums;
 
@@ -5,13 +8,19 @@ namespace FinanceManagement.Domain.Entities;
 
 public class User : BaseEntity
 {
+    public string? UserName { get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;
+    public Gender Gender { get; set; }
+    public string? EmergencyMobileNumber { get; set; }
     public UserRole Role { get; set; }
     public bool IsActive { get; set; } = true;
-    
+
+    //Navigation Properties
     public Partner? Partner { get; set; }
     public Employee? Employee { get; set; }
+
+    //public Profile? Profile {get; set; }
 }
