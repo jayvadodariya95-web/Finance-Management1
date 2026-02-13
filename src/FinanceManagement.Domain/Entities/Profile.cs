6eb1c8603249864fb008105f6361cd2532b983de
Mandal <mandal.nishar@bromcom.com>
2026-02-13 14:12:20 +0530

    Created MonthlySettlement & Profile Model

---
+++ src/FinanceManagement.Domain/Entities/Profile.cs
@@ -0,0 +1,18 @@
+﻿using FinanceManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManagement.Domain.Entities
+{
+    public class Profile : BaseEntity
+    {
+        public int UserId { get; set; }
+        public bool IsPaid { get; set; } = false;
+        public decimal? Amount { get; set; }
+        public User User { get; set; } = null!;
+    }
+
+}
