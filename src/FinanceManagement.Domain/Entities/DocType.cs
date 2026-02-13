6d57a3ed104e65a47b0e83796153a60eeb3fe7fe
ParagWeb7 <paragweb7@gmail.com>
2026-02-13 11:13:54 +0530

    Documents Models are ready

---
+++ src/FinanceManagement.Domain/Entities/DocType.cs
@@ -0,0 +1,15 @@
+﻿using FinanceManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManagement.Domain.Entities
+{
+    class DocType : BaseEntity
+    {
+        public string? TypeName { get; set; }
+        
+    }
+}
