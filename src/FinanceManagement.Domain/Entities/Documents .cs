7a869d4bc39b5ded49a4c731f86619c263a5704d
ParagWeb7 <paragweb7@gmail.com>
2026-02-13 12:48:34 +0530

    Created doctype, documents models.

---
+++ src/FinanceManagement.Domain/Entities/Documents .cs
@@ -0,0 +1,20 @@
+﻿using FinanceManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManagement.Domain.Entities
+{
+   public class Documents : BaseEntity
+    {
+        
+        public int DocType_id { get; set; }
+
+        public string? Link { get; set; }
+
+        //Navigation property
+        public DocType? DocType { get; set; }
+    }
+}
